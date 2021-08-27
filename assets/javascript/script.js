efc73fdcc7bfc77f86a34983610406f6bb533468
ggamb <glenngambardella372@gmail.com>
2021-08-27 10:47:12 -0400

    added some HTML and formatting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,34 @@
 var timerEl = document.getElementById("timer");
 var startButton = document.getElementById("start");
+var answerElOne = document.getElementById("answer-choice-1");
+var answerElTwo = document.getElementById("answer-choice-2");
+var answerElThree = document.getElementById("answer-choice-3");
+var answerElFour = document.getElementById("answer-choice-4");
+var answerElFive = document.getElementById("answer-choice-5");
+
+var questionArray = [
+    questionOne = {
+        question: "What is your favorite color?", 
+        answerChoices: "blue/red/green/orange/purple",
+        a: "red"
+    }, questionTwo = {
+        question: "What is your least favorite color?", 
+        answerChoices: "blue/red/green/orange/purple",
+        a: "blue"
+    }, questionThree = {
+        question: "What is your favorite color?", 
+        answerChoices: "blue/red/green/orange/purple",
+        a: "red"
+    }, questionFour = {
+        question: "What is your favorite color?", 
+        answerChoices: "blue/red/green/orange/purple",
+        a: "red"
+    }, questionFive = {
+        question: "What is your favorite color?", 
+        answerChoices: "blue/red/green/orange/purple",
+        a: "red"
+    }
+];
 
 
 function startTimer() {
@@ -15,5 +44,17 @@ function startTimer() {
         }
     }, 1000);
 };
+
+
+function setAnswerChoices() {
+    
+    for(var i = 0 ; i < questionArray.length; i++){
+        var words = questionArray[i].split("/");
+
+        for(var j = 0; j < words.length; i++){
+            
+        }
+    }
+}
 
 startButton.onclick = startTimer;
