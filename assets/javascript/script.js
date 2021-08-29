94e2c2f1de56a2a1c831aa38b4843e7c28942b4c
ggamb <glenngambardella372@gmail.com>
2021-08-29 14:21:56 -0400

    working on answer selection

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,7 @@ var answerElTwo = document.getElementById("answer-choice-2");
 var answerElThree = document.getElementById("answer-choice-3");
 var answerElFour = document.getElementById("answer-choice-4");
 var answerElFive = document.getElementById("answer-choice-5");
+var answerEls = document.querySelector(".container");
 
 var questionArray = [
     {
@@ -51,17 +52,15 @@ function setAnswerChoices() {
     
     for(var i = 0 ; i < questionArray.length; i++){
         questionEl.textContent = questionArray[i].question;
-        console.log(questionEl.textContent);
 
         splitAnswerChoices(questionArray[i].answerChoices);
 
-        
+        checkAnswer(questionArray[i].a);
     }
-}
+};
 
 function splitAnswerChoices(selectedQuestion) {
     var words = selectedQuestion.split("/");
-    console.log(words);
 
     answerElOne.textContent = words[0];
     answerElTwo.textContent = words[1];
@@ -70,8 +69,30 @@ function splitAnswerChoices(selectedQuestion) {
     answerElFive.textContent = words[4];
 
     return;
-}
+};
 
+function checkAnswer(correctAnswer) {
+    console.log("we got here");
+    
+    var userChoice = getInput(); //fix this somehow
+
+    console.log(userChoice);
+
+    if(correctAnswer === userChoice){
+        console.log("correct!");
+    } else {
+        console.log("false!");
+    }
+};
+
+function getInput(event){
+    var userChoice = event.target.textContent;
+    console.log(userChoice);
+    return userChoice;
+};
+
+
+startButton.onclick = startTimer;
 setAnswerChoices();
 splitAnswerChoices();
-startButton.onclick = startTimer;
+answerEls.addEventListener('click', getInput);
