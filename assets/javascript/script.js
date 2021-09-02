3dbbb15633bfdb192f145ac7e47c80b74a66c619
ggamb <glenngambardella372@gmail.com>
2021-09-02 19:32:45 -0400

    added timer functionality, welcome screen, and beginning of localStorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 //Variables to selected HTML IDs
 var timerEl = document.getElementById("timer");
 var startButton = document.getElementById("start");
+var blockerEl = document.getElementById("block-questions");
+var headerTextEl = document.querySelector(".count-of-three");
 var questionEl = document.getElementById("question");
 var answerElOne = document.getElementById("answer-choice-1");
 var answerElTwo = document.getElementById("answer-choice-2");
@@ -9,6 +11,8 @@ var answerElFour = document.getElementById("answer-choice-4");
 var answerElFive = document.getElementById("answer-choice-5");
 var answerEls = document.querySelector(".container");
 var answerResult = document.getElementById("answer-result");
+var playAgainEl = document.getElementById("goAgain");
+var scoreResultsEl = document.getElementById("score-results");
 
 //Global variable to control which question appears
 var index = 0;
@@ -17,6 +21,9 @@ var index = 0;
 var timeRemaining = 100;
 var increment = 10;
 var decrement = 10;
+
+//Other global variables
+
 
 //Array holding the questions for the quiz. Property answerChoices contains all answers for one question separated by a slash
 var questionArray = [
@@ -43,8 +50,30 @@ var questionArray = [
     }
 ];
 
-//Timer functrion
+//starts joke timer and leads to function to start game timer
+function timerStart() {
+    var count = 1;
+    var timerInterval = setInterval(function(){
+        if(count < 3){
+            startButton.textContent = count;
+            count++;
+        } else if (count === 3){
+            startButton.textContent = 5;
+            count++;
+        } else {
+            timerEl.textContent = "Seconds remaining: " + timeRemaining;
+            clearInterval(timerInterval);
+            startTimer();
+        }
+    }, 1000);
+}
+
+//Gam timer function
 function startTimer() {
+    blockerEl.remove();
+    startButton.remove();
+    headerTextEl.remove();
+
     //Function to decrement and display the timer every 1 second
     var timerInterval = setInterval(function(){
         if(timeRemaining>=0){
@@ -52,18 +81,19 @@ function startTimer() {
             timeRemaining--;
         } else {
             clearInterval(timerInterval);
-            loadScores();
+            getHighScore();
         }
     }, 1000);
 };
 
-//Function to set the answer choices. If no questions remain in the question array, high scores are automatically loaded
+//Function to set the answer choices. If no questions remain in the question array, high scores are automatically loaded to local storage
 function setAnswerChoices() {
+    
     if(index < questionArray.length) {
         questionEl.textContent = questionArray[index].question;
         splitAnswerChoices(questionArray[index].answerChoices);
     } else {
-        loadScores();
+        getHighScore();
     }
 
 };
@@ -72,7 +102,7 @@ function setAnswerChoices() {
 function splitAnswerChoices(selectedQuestion) {
     var words = selectedQuestion.split("/");
 
-    answerElOne.textContent = words[0];
+    answerElOne.textContent = words[0]; //Make more scalable??
     answerElTwo.textContent = words[1];
     answerElThree.textContent = words[2];
     answerElFour.textContent = words[3];
@@ -94,23 +124,42 @@ function getInput(event){
         console.log("correct!");
     } else {
         timeRemaining -= decrement;
-        answerResult.textContent = "Question " + (index+1) + " is incorrect!" + decrement + " seconds deducted!"; //add CSS
+        answerResult.textContent = "Question " + (index+1) + " is incorrect! " + decrement + " seconds deducted!";
         answerEls.appendChild(answerResult);
         console.log("false!");
     }
 
-    //Increases index after the first passthrough and calls setAnswerChoices() to set up the next question
+    //Increases index after the passthrough and calls setAnswerChoices() to set up the next question
     index++;
     setAnswerChoices();
 };
 
 //Loads the webpage with user's high scores
-function loadScores(){
-    window.location.href = "./assets/html/score.html";
+function getHighScore(){    
+    var highScore = localStorage.getItem("highscore");
+    var score = timeRemaining;
+    if(highScore !== null){
+        if (score > highScore) {
+            localStorage.setItem("highscore", score);      
+        }
+    } else{
+        localStorage.setItem("highscore", score);
+    }
+    console.log(highScore);
+    console.log(score);
+    if(scoreResultsEl){
+        scoreResultsEl.textContent = "High Score: " + localStorage.getItem("highscore");
+    }
+    
+    loadScores();
 };
 
+function loadScores() {
+    window.location.href = "./assets/html/score.html";
+}
+
 //Listeners and functions to start posting questions
-startButton.onclick = startTimer;
+startButton.onclick = timerStart;
 setAnswerChoices();
 //splitAnswerChoices();
 answerEls.addEventListener('click', getInput);
